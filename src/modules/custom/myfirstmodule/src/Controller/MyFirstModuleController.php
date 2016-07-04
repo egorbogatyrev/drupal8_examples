449800f59d93c139eb29948bf8ce751fda5fe1d2
Egor Bogatyrev <Egor_Bogatyrev@epam.com>
2016-07-04 12:32:01 +0300

    Refactoring myfirstmodule.

--- src/modules/custom/myfirstmodule/src/Controller/MyFirstModuleController.php
+++ src/modules/custom/myfirstmodule/src/Controller/MyFirstModuleController.php
@@ -1,13 +1,29 @@
 <?php
 
+/**
+ * @file
+ * Controller class file for MyFirstModule module.
+ */
+
 namespace Drupal\myfirstmodule\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Class MyFirstModuleController
+ *
+ * @package Drupal\myfirstmodule\Controller
+ */
 class MyFirstModuleController extends ControllerBase {
 
+  /**
+   * SayHello method.
+   *
+   * @return mixed
+   *   Array of content properties.
+   */
   public function sayHello() {
-    $output['#title'] = 'SayHello page';
-    $output['#markup'] = 'Common guy! Say Hello!';
+    $output['#title']  = 'SayHello page';
+    $output['#markup'] = 'Come on guy! Say Hello!';
     return $output;
   }
 }
