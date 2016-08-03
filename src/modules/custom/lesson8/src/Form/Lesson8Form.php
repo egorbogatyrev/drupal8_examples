891967ec4496f0fc5bb1213a39bd7bf9abf2489e
e.bogatyrev <php.sql.developer@gmail.com>
2016-08-03 11:16:37 +0200

    Updated the lesson8 cache & logging.

--- src/modules/custom/lesson8/src/Form/Lesson8Form.php
+++ src/modules/custom/lesson8/src/Form/Lesson8Form.php
@@ -16,6 +16,8 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class Lesson8Form extends FormBase {
 
+  const LESSON8_CID = 123;
+
   /**
    * {@inheritdoc}
    */
@@ -27,6 +29,15 @@ class Lesson8Form extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $cache = \Drupal::cache('lesson8')->get($this::LESSON8_CID, TRUE);
+    $message = t('There are no any cache items');
+    if ($cache) {
+      $is_valid = $cache->valid;
+      $message = 'Cache item: ';
+      $message .= $cache->data . ' - ' . ($is_valid ? 'valid' : 'invalid');
+    }
+    drupal_set_message($message);
+
     $form['message'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Type a message'),
@@ -35,7 +46,21 @@ class Lesson8Form extends FormBase {
 
     $form['save'] = [
       '#type'  => 'submit',
-      '#value' => $this->t('Send'),
+      '#value' => $this->t('Save message in log & cache'),
+    ];
+
+    $form['invalidate'] = [
+      '#type'  => 'submit',
+      '#submit' => ['::invalidateCacheSubmit'],
+      '#limit_validation_errors' => [],
+      '#value' => $this->t('Invalidate cache'),
+    ];
+
+    $form['delete'] = [
+      '#type'  => 'submit',
+      '#submit' => ['::deleteCacheSubmit'],
+      '#limit_validation_errors' => [],
+      '#value' => $this->t('Delete cache'),
     ];
 
     return $form;
@@ -46,10 +71,31 @@ class Lesson8Form extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $message = $form_state->getValue('message');
-
-//    \Drupal::service('lesson8.logger')->log(RfcLogLevel::CRITICAL, $message);
-
+    \Drupal::cache('lesson8')->set($this::LESSON8_CID, $message);
     \Drupal::service('lesson8.service_to_log_to_multiple_channels')->logToOtherChannels($message);
+    drupal_get_messages();
+  }
+
+  /**
+   * Invalidate cache submit callback.
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
+  public function invalidateCacheSubmit(array &$form, FormStateInterface $form_state) {
+    \Drupal::cache('lesson8')->invalidate($this::LESSON8_CID);
+    drupal_get_messages();
+  }
+
+  /**
+   * Invalidate cache submit callback.
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
+  public function deleteCacheSubmit(array &$form, FormStateInterface $form_state) {
+    \Drupal::cache('lesson8')->delete($this::LESSON8_CID);
+    drupal_get_messages();
   }
 }
 
