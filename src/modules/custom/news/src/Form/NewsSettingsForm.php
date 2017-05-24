e36250b94ed5cab46674e5e3642d875b23e37e7d
Egor Bogatyrev <Egor_Bogatyrev@epam.com>
2017-05-24 11:51:01 +0300

    NewsExperiment: new form elements.

--- src/modules/custom/news/src/Form/NewsSettingsForm.php
+++ src/modules/custom/news/src/Form/NewsSettingsForm.php
@@ -5,6 +5,7 @@ namespace Drupal\news\Form;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Url;
 
 /**
@@ -39,6 +40,8 @@ class NewsSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    drupal_set_message(t('Drupal test message.'));
+
     $form = parent::buildForm($form, $form_state);
     // HTML 5 elements.
     $form['tel'] = [
@@ -73,7 +76,11 @@ class NewsSettingsForm extends ConfigFormBase {
       '#max' => 100,
       '#step' => 50,
     ];
-    ##################################
+
+    $form['separator'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'hr',
+    ];
     // New other elements.
     $form['color'] = [
       '#type' => 'color',
@@ -105,7 +112,7 @@ class NewsSettingsForm extends ConfigFormBase {
     $form['html_tag'] = [
       '#type' => 'html_tag',
       '#tag' => 'div',
-      '#value' => $this->t('HtmlTag'),
+      '#value' => $this->t('This text is wrapped by html_tag'),
     ];
     $form['inline_template'] = [
       '#type' => 'inline_template',
@@ -116,28 +123,49 @@ class NewsSettingsForm extends ConfigFormBase {
     ];
     $form['language_select'] = [
       '#type' => 'language_select',
-      '#title' => $this->t('language_select'),
+      '#title' => $this->t('Language Select'),
+      '#languages' => LanguageInterface::STATE_CONFIGURABLE | LanguageInterface::STATE_SITE_DEFAULT,
+      //'#languages' => LanguageInterface::STATE_ALL,
     ];
     $form['more_link'] = [
       '#type' => 'more_link',
       '#url' => Url::fromUri('https:yandex.ru'),
     ];
+
+    $links['delete'] = [
+      'title' => $this->t('Delete'),
+      'url' => Url::fromUri('https:yandex.ru'),
+    ];
+    $links['delete_items'] = [
+      'title' => $this->t('Delete items'),
+      'url' => Url::fromUri('https:yandex.ru'),
+    ];
+    $links['update'] = [
+      'title' => $this->t('Update items'),
+      'url' => Url::fromUri('https:yandex.ru'),
+    ];
     $form['operations'] = [
       '#type' => 'operations',
+      '#links' => $links,
       '#title' => $this->t('operations'),
     ];
+
     $form['page'] = [
       '#type' => 'page',
-      '#title' => $this->t('page'),
+      '#show_messages' => FALSE,
+      'content' => 'THIS IS PAGE',
     ];
+
     $form['status_messages'] = [
       '#type' => 'status_messages',
-      '#title' => $this->t('status_messages'),
     ];
+
     $form['status_report'] = [
       '#type' => 'status_report',
-      '#title' => $this->t('status_report'),
+      '#requirements' => 'djasjdlkajsdljsalkl',
+      '#prefix' => $this->t('status_report'),
     ];
+
     $form['system_compact_link'] = [
       '#type' => 'system_compact_link',
       '#title' => $this->t('system_compact_link'),
